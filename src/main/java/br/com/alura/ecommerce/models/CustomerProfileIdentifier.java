5ea77bd55715ebedcad8ec283b19c0d842fa0524
alexandredgns <alexandredgns@gmail.com>
2025-03-25 16:24:22 -0300

    Tokens, Models and Costs

---
+++ src/main/java/br/com/alura/ecommerce/models/CustomerProfileIdentifier.java
@@ -0,0 +1,84 @@
+package br.com.alura.ecommerce.models;
+
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CustomerProfileIdentifier {
+    public static void main(String[] args) {
+
+            var system = """
+                    Identifique o perfil de compra de cada cliente
+                    
+                    A resposta deve ser:
+                    
+                    Cliente - descreva o perfil do cliente em tres palavras
+                    """;
+
+            var user = getClientsFromFile("lista_de_compras_100_clientes.csv");
+
+        var tokenCount = countTokens(user);
+        System.out.println("Token Count: " + tokenCount);
+
+        sendRequest(user, system);
+
+//        var model = "gpt-3.5-turbo";
+//        var sizeOfExpectedResponse = 2048;
+//
+//        if(tokenCount > 4096 - sizeOfExpectedResponse) {
+//            model = "gpt-3.5-turbo-16k";
+//        }
+
+//        System.out.println("Model: " + model);
+
+//        sendRequest(user, system, model, sizeOfExpectedResponse);
+
+    }
+
+    public static void sendRequest(String user, String system) {
+
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_KEY"), Duration.ofSeconds(90));
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4o-mini")
+//                .maxTokens(sizeOfExpectedResponse)
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value() , system)
+                ))
+                .temperature(1.0)
+                .build();
+
+        service
+                .createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(c -> System.out.println(c.getMessage().getContent()));
+    }
+
+    private static String getClientsFromFile(String file) {
+        try {
+            var path = Path.of(ClassLoader
+                    .getSystemResource(file)
+                    .toURI());
+            return Files.readAllLines(path).toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Wasn't possible to load the file");
+        }
+    }
+
+    private static int countTokens(String prompt) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_3_5_TURBO);
+        return enc.countTokens(prompt);
+    }
+
+}
---
+++ src/main/java/br/com/alura/ecommerce/models/TokenCounter.java
@@ -0,0 +1,24 @@
+package br.com.alura.ecommerce.models;
+
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.*;
+
+import java.math.BigDecimal;
+
+public class TokenCounter {
+
+    public static void main(String[] args) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_3_5_TURBO);
+        var countedTokens = enc.countTokens("Identifique o perfil de compra de cada cliente");
+
+        System.out.println("Counted tokens: " + countedTokens);
+
+        var cost = new BigDecimal(countedTokens)
+                .divide(new BigDecimal("1000"))
+                .multiply(new BigDecimal("0.0010"));
+
+        System.out.println("Cost: $" + cost);
+
+    }
+}
